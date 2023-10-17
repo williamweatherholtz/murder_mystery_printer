e92a55ccd72c27cc4ff592d0fc2251c98f35ac85
William Weatherholtz <williamweatherholtz@gmail.com>
2023-10-16 20:59:25 -0400

    removed_puzzles

--- puzzles/binary.py
+++
@@ -1,4 +0,0 @@
-from numpy import binary_repr
-
-
-print (binary_repr(9))
--- puzzles/german.py
+++
@@ -1,6 +0,0 @@
-#Can you lift 3000 pounds?  Are your children older than you?  Is your skin always blue? Answer me!
-
-# in german: Kannst du 3000 pfund heben? Sind ihre kinder älter als sie? Ist ihre haut immer blau? Gib mir eine antwort!
-
-# answer: no (nein)
-
